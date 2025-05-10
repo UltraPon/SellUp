43ed4b15171c98a8369964f498326f720796888a
comas <comas@HATE-U>
2025-05-10 21:19:37 +0300

    Fix gunicorn startup

--- SellUp/settings.py
+++ SellUp/settings.py
@@ -26,6 +26,9 @@ environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure--%4&+ul9xtn&uu3e1-qsz^)vagpyjn4z8w43bpe4@vfnj%pic+'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
