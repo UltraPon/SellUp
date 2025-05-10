fec532b19e83aeceab1389ad578fdd4a6908df19
comas <comas@HATE-U>
2025-05-10 21:55:49 +0300

    Fix gunicorn startup

--- SellUp/urls.py
+++ SellUp/urls.py
@@ -34,5 +34,4 @@ urlpatterns = [
     path('api/test/', test_api),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
